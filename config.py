332e3f8cca863efdd070921da218858efce52a66
tegic <maksimkruglik2@gmail.com>
2025-05-27 20:03:54 +0200

    constructing main.py

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 from array_with_unique_values import create_array_with_unique_values
 
+results = []
 min_value = 0
 max_value = 10000
 arrays = []
-for i in range(1000, 10001, 1000):
+for i in range(1000, 5001, 250):
     arrays.append(create_array_with_unique_values(i, min_value, max_value))
+    print(f'created array with {i} elements')
--- main.py
+++ main.py
@@ -1,49 +1,77 @@
 from list_classes import List
 from binary_search_tree import BinarySearchTree, TreeElement
 from time import time_ns
-from config import arrays
+from config import arrays, results
 from create_avl import create_avl
-
-"""
-DONE!!!!!!!!!!!!
-БАМБАЙЛЕЙЛА)))))))))))
-"""
-
-my_list = List()
-my_BST = BinarySearchTree()
-
-for i in range(len(arrays[0])):
-    my_BST.add(TreeElement(arrays[0][i]))
-    # for i in range(len(array)):
-    #     my_list.add_sorted(array[i])
-    #     my_BST.add(TreeElement(array[i]))
-
-print(f"HEIGHT {my_BST.find_height()} for {len(arrays[0])} elements")
-my_AVL = BinarySearchTree()
-my_AVL.add(create_avl(my_BST.get_inorder()))
-print(f"AVL HEIGHT {my_AVL.find_height()} for {len(arrays[0])} elements")
-
-# my_BST.print()
-
-# for i in range(len(array) - 1, 0, -1):
-#     print(f"deleting {array[i]}")
-#     my_BST.delete(array[i])
-#     my_BST.print()
-
-# cl = 0
-# ct = 0
-
-# start_cl = time_ns()
-# for i in range(len(array)):
-#     if my_BST.find(array[i]) != False:
-#         ct += 1
-# print(f"ct: {ct} {(time_ns() - start_cl) * 10 ** -9}s")
-
-# start_ct = time_ns()
-# for i in range(len(array)):
-#     cl += my_list.find(array[i])
-# print(f"cl: {cl} {(time_ns() - start_ct) * 10 ** -9}s")
+import pprint
+import math
+import pandas as pd
 
 
-# my_list.print()
-# my_BST.print()
+# LIST
+for i in range(len(arrays)):
+    my_list = List()
+    print(f'add_to_list {len(arrays[i])} elements')
+    s_time = time_ns()
+    for j in range(len(arrays[i])):
+        my_list.add_sorted(arrays[i][j])
+    results.append(['add_to_list_time', len(arrays[i]), round((time_ns() - s_time) * 10 ** -9, 5)])
+    
+    print(f'find_in_list {len(arrays[i])} elements')
+    s_time = time_ns()
+    for j in range(len(arrays[i])):
+        my_list.find(arrays[i][j])
+    results.append(['find_in_list_time', len(arrays[i]), round((time_ns() - s_time) * 10 ** -9, 3)])
+
+    print(f'delete_in_list {len(arrays[i])} elements')
+    s_time = time_ns()
+    for j in range(len(arrays[i])):
+        my_list.find(arrays[i][j])
+    results.append(['delete_in_list_time', len(arrays[i]), round((time_ns() - s_time) * 10 ** -9, 3)])
+    
+
+
+# BST
+for i in range(len(arrays)):
+    my_BST = BinarySearchTree()
+    print(f'add_to_BST {len(arrays[i])} elements')
+    s_time = time_ns()
+    for j in range(len(arrays[i])):
+        my_BST.add(TreeElement(arrays[i][j]))
+    results.append(['add_to_BST_time', len(arrays[i]), round((time_ns() - s_time) * 10 ** -9, 3)])
+    
+    print(f'find_in_BST {len(arrays[i])} elements')
+    s_time = time_ns()
+    for j in range(len(arrays[i])):
+        my_BST.find(arrays[i][j])
+    results.append(['find_in_BST_time', len(arrays[i]), round((time_ns() - s_time) * 10 ** -9, 3)])
+
+    print(f'delete_in_BST {len(arrays[i])} elements')
+    s_time = time_ns()
+    for j in range(len(arrays[i]) - 1, 0, -1):
+        my_BST.find(arrays[i][j])
+    results.append(['delete_in_BST_time',  len(arrays[i]), round((time_ns() - s_time) * 10 ** -9, 3)])
+
+
+# AVL
+for i in range(len(arrays)):
+    my_BST = BinarySearchTree()
+    my_AVL = BinarySearchTree()
+    print(f'add_to_BST {len(arrays[i])} elements')
+    s_time = time_ns()
+    for j in range(len(arrays[i])):
+        my_BST.add(TreeElement(arrays[i][j]))
+    results.append(['get_height_of_BST', len(arrays[i]), my_BST.find_height()])
+    
+    my_AVL.first_el = create_avl(my_BST.get_inorder())
+    results.append(['get_height_of_AVL', len(arrays[i]), my_AVL.find_height()])
+    
+
+df1 = pd.DataFrame(results, columns=['Operation', 'Elements count', 'Value'])
+
+with pd.ExcelWriter('Result.xlsx', engine='xlsxwriter') as writer:
+    df1.to_excel(writer, sheet_name='Sheet 1', index=False)
+
+    workbook = writer.book
+    worksheet = writer.sheets['Sheet 1']
+
